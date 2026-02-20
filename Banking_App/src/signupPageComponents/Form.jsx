d679c7a7aa77d310ceb003cd311637e31e47216a
JBVinoth333 <bevinoth699@gmail.com>
2026-02-20 14:59:58 +0530

    Request code added in Form

--- Banking_App/src/signupPageComponents/Form.jsx
+++ Banking_App/src/signupPageComponents/Form.jsx
@@ -3,6 +3,33 @@ import Textarea from "../common/TextArea";
 import Button from "../common/Button";
 
 export default function Form() {
+        function createAccount(){
+        console.log("Button Clicked");
+        fetch("http://localhost:8080/BankingApplication/SignupServlet", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name: document.getElementById("name").value,
+                aadhaar: document.getElementById("aadhaar").value,
+                yob: Number(document.getElementById("yob").value),
+                mobile: document.getElementById("mobile").value,
+                email: document.getElementById("email").value,
+                address: document.getElementById("address").value,
+                balance: Number(document.getElementById("balance").value),
+                login: document.getElementById("login").value,
+                confirm: document.getElementById("confirm").value
+            })
+        }).then(response => response.json())
+        .then(data => {
+            console.log(data);
+            alert("Account Created Successfully!");
+        }).catch(error => {
+            console.error(error);
+            alert("Error creating account. Please try again.");
+        });
+    }
     return (
         <form action="" id="signup_form">
             <div id="signup_box">
@@ -15,10 +42,9 @@ export default function Form() {
                 <Input id="balance" label="Opening Balance:" type="number" isRequired={true}/>
                 <Input id="login" label="6 Digit Login Number:" type="password" isRequired={true} maxLength="6"/>
                 <Input id="confirm" label="Confirm Login Number:" type="password" isRequired={true} maxLength="6"/>
-                <Button id="btn_createAccount" text="Submit" type="submit"/>
+                <Button id="btn_createAccount" text="Submit" type="submit" onClick={createAccount}/>
             </div>
         </form>
-
     );
 }
 
